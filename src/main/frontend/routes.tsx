595bd66e67b008ded422a4aaff121f284eacda54
start.vaadin.com <no-reply@vaadin.com>
2024-04-08 17:10:59 +0000

    Generated project

---
+++ src/main/frontend/routes.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter, RouteObject } from 'react-router-dom';
+
+const routing = [] as RouteObject[];
+
+export const routes = routing;
+export default createBrowserRouter(routes);
